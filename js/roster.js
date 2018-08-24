77ed01358da8c6a0294757d80946388d212041e8
Daryl <daryl@Daryls-MBP.fritz.box>
2018-08-24 21:15:07 +0200

    test

--- js/roster.js
+++ js/roster.js
@@ -136,7 +136,7 @@ function listMembers(rsp) {
         $('#status-' + memberId).text('Offline').removeClass('online');
       }
 
-      sortMembers(online); // sort members by join date
+      sortMembers(joined); // sort members by join date
 
     }
 
